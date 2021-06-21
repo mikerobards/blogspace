67feaeb4601567d706d4cd2cef61a891d4d03fb5
Michael Robards <mikerobards@icloud.com>
2021-06-21 16:11:43 -0400

    Bob's solution

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ Hints:
 * Loop over the items creating a string of HTML elements you 
   can then put into the div with `innerHTML`
 */
-const container = document.querySelector('.container')
+const blogList = document.getElementById('blog-list')
 
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
@@ -32,14 +32,23 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(data => {
         const postsArr = data.slice(0, 5)
         console.log(postsArr)
-        postsArr.forEach(post => {
-            container.innerHTML += `
-            title: ${post.title} 
-            <br>
-            body: ${post.body}
-            <br>
-            <br>
+        let html = ''
+        for (let post of postsArr) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
             `
-        })
+        }
+        blogList.innerHTML = html
+        // postsArr.forEach(post => {
+        //     container.innerHTML += `
+        //     title: ${post.title} 
+        //     <br>
+        //     body: ${post.body}
+        //     <br>
+        //     <br>
+        //     `
+        // })
     })
 
