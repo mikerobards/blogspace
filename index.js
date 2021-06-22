b38593b6b49489664575c58a28dbb8012d50743b
Michael Robards <mikerobards@icloud.com>
2021-06-22 11:48:48 -0400

    Bob's solution

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 
 
 const blogList = document.getElementById('blog-list')
-let form = document.getElementById('post')
+let form = document.getElementById('new-post')
 let title = form.elements['post-title']
 let body = form.elements['post-body']
 
@@ -36,12 +36,22 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     })
 
 form.addEventListener('submit', (e) => {
-    let title = form.elements['post-title']
-    let body = form.elements['post-body']
-    let postTitle = title.value
-    let postBody = body.value
-    console.log(`title: ${postTitle}; body: ${postBody}`)
-    e.preventDefault();
+    // let title = form.elements['post-title']
+    // let body = form.elements['post-body']
+    // let postTitle = title.value
+    // let postBody = body.value
+    // console.log(`title: ${postTitle}; body: ${postBody}`)
+
+    e.preventDefault()
+    const postTitle = document.getElementById('post-title').value
+    const postBody = document.getElementById('post-body').value
+    // console.log(postTitle, postBody)
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    console.log(data)
+
 })
 
 
